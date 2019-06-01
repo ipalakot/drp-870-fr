96e450cec9b4d2a967aaf6768a60ca1498ae03ce
ipalakot <ipalakot@gmail.com>
2019-06-01 10:12:16 +0200

    Debugging

---
+++ sites/default/files/php/twig/5cec42118e86c___string_template__65649b_s9al6BHE1DycOpzhSDnY3A7sl/XFrwonFxqHXuA_NuC9wc90ED_-oC_LM2fiZrEuUOBrc.php
@@ -0,0 +1,80 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__65649bba9ecd36bc81969e10f02c1bc9bc51e3b58f2ef2d6f7b1e138a3b69386 */
+class __TwigTemplate_879829d900f18a96c7a0d6443f3ad33fdcc9416dd484a43523357129c249b7fc extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = [];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "destination=/drupal/drupal-870-fr/admin/workbench";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__65649bba9ecd36bc81969e10f02c1bc9bc51e3b58f2ef2d6f7b1e138a3b69386";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__65649bba9ecd36bc81969e10f02c1bc9bc51e3b58f2ef2d6f7b1e138a3b69386", "");
+    }
+}
