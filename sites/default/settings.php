d8f99eb604994132f84ca57a81c7f59efb8e22c2
ipalakot <ipalakot@gmail.com>
2019-06-01 10:20:25 +0200

    fin debugage

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -773,10 +773,10 @@ $settings['entity_update_backup'] = TRUE;
  *
  * Keep this code block at the end of this file to take full effect.
  */
-#
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
+
+ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+   include $app_root . '/' . $site_path . '/settings.local.php';
+ }
 $databases['default']['default'] = array (
   'database' => 'drp870eng',
   'username' => 'root',
