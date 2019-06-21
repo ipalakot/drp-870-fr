3d35d4ab065d416c9ebbc0c1582d9e162d1de5b7
ipalakot <ipalakot@gmail.com>
2019-06-21 07:54:08 +0200

    Sending

---
+++ sites/default/files/php/twig/5d0a301aa145a_menu--footer.html.twig_bSKP8V9XlZQ4sP5symH66DV2K/ctpi22IJIlPVE2rqoWTNqk12mwjgFsbF-FqWRXJPEpA.php
@@ -0,0 +1,199 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/mydrupal8theme/templates/menu--footer.html.twig */
+class __TwigTemplate_4c95243a452036856883ed4feaf5c2580db6148d2427a09e4675bf5de153a865 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["import" => 23, "macro" => 31, "if" => 36, "for" => 43];
+        $filters = ["escape" => 38];
+        $functions = ["link" => 50];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['import', 'macro', 'if', 'for'],
+                ['escape'],
+                ['link']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 23
+        $context["menus"] = $this;
+        // line 24
+        echo "
+";
+        // line 29
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar($context["menus"]->getmenu_links(($context["items"] ?? null), ($context["attributes"] ?? null), 0));
+        echo "
+
+";
+    }
+
+    // line 31
+    public function getmenu_links($__items__ = null, $__attributes__ = null, $__menu_level__ = null, ...$__varargs__)
+    {
+        $context = $this->env->mergeGlobals([
+            "items" => $__items__,
+            "attributes" => $__attributes__,
+            "menu_level" => $__menu_level__,
+            "varargs" => $__varargs__,
+        ]);
+
+        $blocks = [];
+
+        ob_start();
+        try {
+            // line 32
+            echo "<div class=\"row\">    
+  ";
+            // line 33
+            $context["menus"] = $this;
+            // line 34
+            echo "  
+
+  ";
+            // line 36
+            if (($context["items"] ?? null)) {
+                // line 37
+                echo "    ";
+                if ((($context["menu_level"] ?? null) == 0)) {
+                    // line 38
+                    echo "      <ul";
+                    echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null)), "html", null, true);
+                    echo ">
+          ";
+                } else {
+                    // line 40
+                    echo "            <ul>
+          ";
+                }
+                // line 42
+                echo "          
+          ";
+                // line 43
+                $context['_parent'] = $context;
+                $context['_seq'] = twig_ensure_traversable(($context["items"] ?? null));
+                foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                    // line 44
+                    echo "            ";
+                    if ((($context["menu_level"] ?? null) == 0)) {
+                        // line 45
+                        echo "                <li class=\"small-6 medium-3 large-2 columns\">
+                    <h4> ";
+                        // line 46
+                        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "title", [])), "html", null, true);
+                        echo "</h4>
+
+            ";
+                    } else {
+                        // line 49
+                        echo "                <li";
+                        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "attributes", [])), "html", null, true);
+                        echo ">
+                  ";
+                        // line 50
+                        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->env->getExtension('Drupal\Core\Template\TwigExtension')->getLink($this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "title", [])), $this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "url", []))), "html", null, true);
+                        echo "
+            ";
+                    }
+                    // line 52
+                    echo "            
+            ";
+                    // line 53
+                    if ($this->getAttribute($context["item"], "below", [])) {
+                        // line 54
+                        echo "               ";
+                        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar($context["menus"]->getmenu_links($this->getAttribute($context["item"], "below", []), ($context["attributes"] ?? null), (($context["menu_level"] ?? null) + 1)));
+                        echo "
+            ";
+                    }
+                    // line 56
+                    echo "                </li> 
+   
+        ";
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+                $context = array_intersect_key($context, $_parent) + $_parent;
+                // line 59
+                echo "        </ul>
+  ";
+            }
+            // line 61
+            echo "  </div>    
+";
+        } catch (\Exception $e) {
+            ob_end_clean();
+
+            throw $e;
+        } catch (\Throwable $e) {
+            ob_end_clean();
+
+            throw $e;
+        }
+
+        return ('' === $tmp = ob_get_clean()) ? '' : new Markup($tmp, $this->env->getCharset());
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/mydrupal8theme/templates/menu--footer.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  157 => 61,  153 => 59,  145 => 56,  139 => 54,  137 => 53,  134 => 52,  129 => 50,  124 => 49,  118 => 46,  115 => 45,  112 => 44,  108 => 43,  105 => 42,  101 => 40,  95 => 38,  92 => 37,  90 => 36,  86 => 34,  84 => 33,  81 => 32,  67 => 31,  60 => 29,  57 => 24,  55 => 23,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/custom/mydrupal8theme/templates/menu--footer.html.twig", "C:\\wamp64\\www\\drupal\\drupal-870-fr\\themes\\custom\\mydrupal8theme\\templates\\menu--footer.html.twig");
+    }
+}
